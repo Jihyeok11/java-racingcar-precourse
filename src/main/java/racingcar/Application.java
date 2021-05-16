d16b680de809cbade529f6b340091a107a269fce
Jihyeok11 <woah68@gmail.com>
2021-05-16 23:12:09 +0900

    입력 값들 설정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,20 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String stringInput = scanner.next();
+        List<String> playerList = Arrays.asList(stringInput.split(","));
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = scanner.nextInt();
+//        System.out.println(playerList);
+//        System.out.println(count);
         // TODO 구현 진행
     }
 }
