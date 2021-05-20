6eaa23d4ffdc2a7de9570134eeae4e52fd6db5b3
Jaewan <woah68@gmail.com>
2021-05-21 02:05:55 +0900

    InputView.java indent 변경

--- src/main/java/views/InputView.java
+++ src/main/java/views/InputView.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public static String Start(Scanner scanner){
+    public static String Start(Scanner scanner) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String stringInput = scanner.next();
         return stringInput;
     }
 
-    public static void Try(){
+    public static void Try() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 }
